14aed0342d711a62472200e3f137220209c6a023
sneheshdutta <sneheshdutta@gmail.com>
2020-08-30 12:32:13 +0530

    new program

--- Assignments/Candel Problem/candles.c
+++ Assignments/Candel Problem/candles.c
@@ -1,7 +1,22 @@
-#include <stdio.h>
-
-    int main()
-    {
-        printf("Hello world!");
-        return(0);
-    }
+#include<stdio.h>
+int main()
+{
+	int t, n,i,ar[100010],s=0,sum;
+	scanf("d",&t);
+	while(t>0)
+	{
+		scanf("%d",&n);
+		for(i=0;i<n;i++)
+		{
+			scanf("%d",&ar[i]);
+		}
+		sum=ar[0];
+		for(i=1;i<n;i++)
+		{
+			sum+=ar[i];
+			s+=sum;
+		}
+		printf("%d",s);
+		t--;
+	}
+}
