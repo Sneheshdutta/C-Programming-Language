0149bd42067c72c6bf07fbe4e45486e2476c178c
sneheshdutta <sneheshdutta@gmail.com>
2020-09-05 20:32:03 +0530

    fixed

--- Practice Programs/checkleapyear.c
+++ Practice Programs/checkleapyear.c
@@ -4,13 +4,18 @@ int main ()
     int y;
     printf("Enter the Year:");
     scanf("%d",&y);
-    if((y%100==0) && (y%400==0))
-        printf("%d is a Leapyear",y);
-    else if(y%4==0)
-            printf("%d is a Leap year",y);
-        else
-        {
-            printf("%d is not a Leapyear",y);
-        }
-    return 0;    
+	if (y%4==0){
+		if (y%400==0){
+			printf("%d is a Leapyear",y);
+		}
+		else if (y%100==0){
+			printf("%d is not a Leapyear",y);
+		}
+		else{
+			printf("%d is a Leapyear",y);
+		}
+	}
+	else{
+		printf("%d is not a Leapyear",y);
+	}   
 }
