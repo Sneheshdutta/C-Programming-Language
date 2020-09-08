9afb0b04f9495f1c3380eb96fa7096b585744cbe
sneheshdutta <sneheshdutta@gmail.com>
2020-09-09 02:17:40 +0530

    new

---
+++ Practice Programs/tempCodeRunnerFile.c
@@ -0,0 +1,1 @@
+rn &arr; } int main(){ unsigned long int (*ptr)[5]; ptr = function(); printf("%d", *(*ptr + 4)); return 0; }
--- Practice Programs/test.c
+++ Practice Programs/test.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 int main()
-{
- int main() {
- int k=30;
- k = function1(k=function1(k=function1(k))); printf("k=%d ", k);
- return 0;
- }
- int function1(int k)
- {
- k++;
- return k;
- }
+{ 
+int i = 3; 
+int *j; 
+int **k; 
+j = &i; 
+k = &j; 
+k++; 
+printf("%d ",**k); 
+return 0; 
 }
