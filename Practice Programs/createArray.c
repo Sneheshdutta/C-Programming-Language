d85eaebeb58bc16d64b1738141a12297e4f4d6d4
sneheshdutta <sneheshdutta@gmail.com>
2020-09-18 22:15:30 +0530

    array fixed

--- Practice Programs/createArray.c
+++ Practice Programs/createArray.c
@@ -6,12 +6,12 @@ int a[60];
 printf("Enter the total number of elements must be below 60:");
 scanf("%d",&x);
 printf("Enter the eloements of array:");
-for(i=0;i<x;i++);
+for(i=0;i<x;i++)
     {
         scanf("%d",&a[i]);
     }
 for(i=0;i<x;i++)
     {
-        printf("%d",&a[i]);
+        printf("%d",a[i]);
     }
 }
